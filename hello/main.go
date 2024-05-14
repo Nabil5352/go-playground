1b995287c3cc5651c48f56b45104af1fbd8fa055
nabil5352 <report.nabil@gmail.com>
2024-05-14 08:36:14 +0600

    added array and slices example

--- hello/main.go
+++ hello/main.go
@@ -30,4 +30,54 @@ func main() {
 	var bigI uint64 = 18446744073709551615
 	fmt.Printf("max range => b: %b smallI: %d bigI: %d\n", b, smallI, bigI)
 	fmt.Printf("adding 1 => b: %b smallI: %d bigI: %d\n", b+1, smallI+1, bigI+1)
+
+	/******************
+	***** Array *******
+	******************/
+
+	/*
+	* Create an array of size 6.
+	* Undeclared values will be zero by default
+	 */
+	var arr = [6]int{2, 1, 3}
+	fmt.Println(arr)
+
+	/*
+	* Create an array with no adjustable size.
+	* Undeclared values will be zero by default
+	 */
+	var unlimited_arr = [...]int{4, 5, 9}
+	fmt.Println(unlimited_arr)
+
+	/*
+	* Array of size 7
+	* first 3 are declared 2,1,3
+	* then, 5: means set 0 until 5th value
+	* set 1 as 6th value
+	* rest (if any) is zero by default
+	 */
+	var new_arr = [7]int{2, 1, 3, 5: 1}
+	fmt.Println(new_arr, len(new_arr))
+
+	/******************
+	***** Slices *******
+	******************/
+
+	// Slices are same as array except here size declaratio isn't mandatory
+	var sl = []int{1, 3, 5, 7, 9}
+	var sl_1 = []int{1, 4: 5, 6: 7}
+	fmt.Println(sl, sl_1)
+
+	// Multidimensional Slice
+	var sl_m_1 = [][]int{{1, 2}, {3, 4}}
+	fmt.Println(sl_m_1[0][0])
+
+	// Append
+	sl_1 = append(sl_1, 8, 9)
+	fmt.Println(sl_1)
+
+	// Append another slice
+	var sl_2 = []int{10, 11, 5: 15}
+	sl_1 = append(sl_1, sl_2...)
+	fmt.Println(sl_1)
 }
